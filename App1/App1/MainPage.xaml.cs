b048642634d5fe11a8cd1e2131cd8c3fb4c21789
juan <jdariasa4@gmail.com>
2019-02-28 17:35:08 -0600

    Avance de tarea

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Presione.Clicked += Presione_Clicked;
+            Digite_Número.Text += Digite_Número;
+            Captura.Text += Captura;
+           
+
+
+
+        }
+
+        
+        
+
+        private void Presione_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Ha dado click");
+            Console.ReadLine(Captura);
+
+        }
+    }
+}
